180e646d7a3a751a353bb633f47d7674430fd3a2
Itay Turgeman <turgey@MacBook-Pro-3.local>
2022-04-23 20:32:49 -0700

    list app

---
+++ list_app_IT/imdb_top_10UITests/imdb_top_10UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  imdb_top_10UITestsLaunchTests.swift
+//  imdb_top_10UITests
+//
+//  Created by Itay Turgeman on 2/11/22.
+//
+
+import XCTest
+
+class imdb_top_10UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
